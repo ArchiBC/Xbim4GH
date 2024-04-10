f55849ffbe4550a53bd10d36bdf16242b5269b74
一 辛 <Archi-BC@outlook.com>
2024-04-10 18:27:56 +0800

    添加设置。下班！！！

---
+++ Xbim4GH/Parameter/IfcEnity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleGrasshopper.Attributes;
+
+namespace Xbim4GH.Parameter;
+
+[Icon("Xbim4GH/Icons/IfcEnity.png")]
+[DocObj("IfcEnity", "IFCEnity", "IFC")]
+public class IfcEnity
+{
+
+}
---
+++ Xbim4GH/Settings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using SimpleGrasshopper.Attributes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xbim4GH;
+
+enum BaseLibraryTag
+{
+    [Description("Xbim")]
+    Xbim,
+    //GeometryGym,
+    //IfcOpenShell
+}
+
+enum IfcVersion
+{
+    [Description("IFC2x3")]
+    IFC2x3,
+    [Description("IFC4")]
+    IFC4,
+    [Description("IFC4x3")]
+    IFC4x3,
+}
+
+internal static partial class Settings
+{
+    /// <summary>
+    /// 设置基础库，目前暂不可切换
+    /// </summary>
+    [Setting]
+    private static readonly BaseLibraryTag baseLibrary = BaseLibraryTag.Xbim;
+
+    /// <summary>
+    /// 设置IFC版本，目前暂不可切换
+    /// </summary>
+    [Setting]
+    private static readonly IfcVersion ifcVersion = IfcVersion.IFC4x3;
+
+}
