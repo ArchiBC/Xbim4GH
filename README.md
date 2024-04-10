1db80c4c35f3e7f768bd89e957e4191e7ba79fef
一 辛 <Archi-BC@outlook.com>
2024-04-10 14:58:03 +0800

    创建项目

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Xbim4GH
+
+通过SimpleGrasshopper转译Xbim的IFC实体解析功能到gh中。
+
+本库基于[SimpleGrasshopper](https://github.com/ArchiDog1998/SimpleGrasshopper) 和 [Xbim.Essentials](https://github.com/xBimTeam/XbimEssentials)
+
---
+++ Xbim4GH/Xbim4GHInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace Xbim4GH
+{
+    public class Xbim4GHInfo : GH_AssemblyInfo
+    {
+        public override string Name => "Xbim4GH";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "通过SimpleGrasshopper转译Xbim的IFC实体解析功能到gh中。";
+
+        public override Guid Id => new Guid("a68f255c-8291-438d-ba99-7176d08181bf");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "ArchiBC";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "Archi=BC@outlook.com";
+    }
+}
